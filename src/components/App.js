815d15b89782b01a67f2605af64e47d40be9e372
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-19 22:18:35 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,23 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [display,setDisplay]=useState(false)
+
+  const change=()=>{
+    setDisplay(true)
+    
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+    
+
+      <button onClick={change}>click me</button>
+
+      {display &&(
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    }
+
     </div>
   );
 }
